ebb6e2e9e3d8e9394369b9e74a6b911fd09c7904
Francesco Zaia <francesco.zaia@gmail.com>
2017-03-31 18:49:20 +0200

    quick test

--- index.js
+++ index.js
@@ -1,50 +1,44 @@
 
-var request = require("request");
-var bodyParser = require('body-parser');
-var path = require('path');
-var crypto = require('crypto');
-var tls = require('tls');
-var express = require('express');
-var fs = require('fs'),
+var request = require('request'),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    crypto = require('crypto'),
+    tls = require('tls'),
+    express = require('express'),
+    fs = require('fs'),
     http = require('http'),
-    https = require('https');
+    https = require('https'),
+    messenger = require('./messenger.js');
+
 var app = express();
 
 var privateKey = fs.readFileSync('/home/gituser/letsencrypt/live/secure.canecanuto.com/privkey.pem').toString();
 var certificate = fs.readFileSync('/home/gituser/letsencrypt/live/secure.canecanuto.com/fullchain.pem').toString();
 var chain = fs.readFileSync('/home/gituser/letsencrypt/live/secure.canecanuto.com/chain.pem').toString();
 
-var options = tls.createSecureContext({
-    key: privateKey,
-    cert: certificate,
-    ca: chain
-});
-var test = {
+var secureOptions = {
  key: privateKey,
  cert: certificate
 }
-var jsonParser = bodyParser.json();
-// parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
-// app.use(bodyParser.json())
-
-
-//app.set('port', (process.env.PORT || 80));
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'up_patriots_to_arms') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
 
-app.post('/webhook', jsonParser, function (req, res) {
-console.log("yo");  
-console.log(req.body);
-console.log("..........");
+app.post('/webhook', bodyParser.json(), function (req, res) {
 	var data = req.body;
-
   // Make sure this is a page subscription
   if (data.object === 'page') {
 
@@ -56,7 +50,7 @@ console.log("..........");
       // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
         if (event.message) {
-          receivedMessage(event);
+          messenger.receivedMessage(event);
         } else {
           console.log("Webhook received unknown event: ", event);
         }
@@ -72,162 +66,8 @@ console.log("..........");
   }
 });
   
-function receivedMessage(event) {
-  // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
-var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
-
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
-
-  var messageId = message.mid;
-
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-
-  if (messageText) {
-
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-      case 'cantami':
-      case 'Cantami':
-        sendCanzone(senderID);
-        break;
-      case 'prova':
-        sendTextMessage(senderID, 'teleGattone');
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
-}
-function sendGenericMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",               
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",               
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
-      }
-    }
-  };  
-
-  callSendAPI(messageData);
-}
-
-function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function sendCanzone(recipientId) {
-  var canzoni = [
-    "Ho avuto molte donne in vita mia, e in ogni camera ho lasciato qualche mia energia.",
-    "E giorni di digiuno e di silenzio, per fare i cori nelle messe tipo Amanda Lear.",
-    "Le barricate in piazza le fai per conto della borghesia, che crea falsi miti di progresso.",
-    "Segnali di vita nei cortili e nelle case all'imbrunire, le luci fanno ricordare le meccaniche celesti."
-  ];
-
-  var canzone = canzoni[Math.floor(Math.random() * canzoni.length)];
-  var messageData = {
-    recipient: {
-	id: recipientId
-    },
-    message: {
-      text: canzone
-    }
-  };
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: 'EAAawiwbXgjMBAD1AsneZBclfVpKiO5tEMmIvOxrro0ahgdicJARxiCg8QKlWgNvBtIrqiwZC4ZC7GwfMschadRdDtalTjFY8G8N9Ar4cRZCinTIAL1CPAZBuLIkQ6k3nrLoq0ncPd90yXuxQm4UsPZBraZCINZAz0GUUYHdD00PhzAZDZD' },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s", 
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });  
-}
-
-app.get('/webhook', function(req, res) {
-console.log(req.query['hub.mode']);
-console.log(req.query['hub.verify_token'])
-  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'up_patriots_to_arms') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
-});
-/*app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})*/
 
 // Create an HTTP service.
 http.createServer(app).listen(80);
 // Create an HTTPS service identical to the HTTP service.
-https.createServer(test, app).listen(443);
+https.createServer(secureOptions, app).listen(443);
---
+++ messenger.js
@@ -0,0 +1,146 @@
+module.exports = {
+  receivedMessage: function (event) {
+    console.log("Message data: ", event.message);
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log("Received message for user %d and page %d at %d with message:",
+      senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    var messageId = message.mid;
+
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+
+    if (messageText) {
+
+      // If we receive a text message, check to see if it matches a keyword
+      // and send back the example. Otherwise, just echo the text we received.
+      switch (messageText) {
+        case 'generic':
+          sendGenericMessage(senderID);
+          break;
+        case 'cantami':
+        case 'Cantami':
+          sendCanzone(senderID);
+          break;
+        default:
+          sendTextMessage(senderID, messageText);
+      }
+    } else if (messageAttachments) {
+      sendTextMessage(senderID, "Message with attachment received");
+    }
+  },
+
+  sendGenericMessage: function (recipientId) {
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            elements: [{
+              title: "rift",
+              subtitle: "Next-generation virtual reality",
+              item_url: "https://www.oculus.com/en-us/rift/",
+              image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+              buttons: [{
+                type: "web_url",
+                url: "https://www.oculus.com/en-us/rift/",
+                title: "Open Web URL"
+              }, {
+                type: "postback",
+                title: "Call Postback",
+                payload: "Payload for first bubble",
+              }],
+            }, {
+              title: "touch",
+              subtitle: "Your Hands, Now in VR",
+              item_url: "https://www.oculus.com/en-us/touch/",
+              image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+              buttons: [{
+                type: "web_url",
+                url: "https://www.oculus.com/en-us/touch/",
+                title: "Open Web URL"
+              }, {
+                type: "postback",
+                title: "Call Postback",
+                payload: "Payload for second bubble",
+              }]
+            }]
+          }
+        }
+      }
+    };
+
+    callSendAPI(messageData);
+  },
+
+  sendTextMessage: function (recipientId, messageText) {
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        text: messageText
+      }
+    };
+
+    callSendAPI(messageData);
+  },
+
+  sendCanzone: function (recipientId) {
+    var canzoni = [
+      "Ho avuto molte donne in vita mia, e in ogni camera ho lasciato qualche mia energia.",
+      "E giorni di digiuno e di silenzio, per fare i cori nelle messe tipo Amanda Lear.",
+      "Le barricate in piazza le fai per conto della borghesia, che crea falsi miti di progresso.",
+      "Segnali di vita nei cortili e nelle case all'imbrunire, le luci fanno ricordare le meccaniche celesti."
+    ];
+
+    var canzone = canzoni[Math.floor(Math.random() * canzoni.length)];
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        text: canzone
+      }
+    };
+    callSendAPI(messageData);
+  },
+
+  callSendAPI: function callSendAPI(messageData) {
+    request({
+      uri: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: 'EAAawiwbXgjMBAD1AsneZBclfVpKiO5tEMmIvOxrro0ahgdicJARxiCg8QKlWgNvBtIrqiwZC4ZC7GwfMschadRdDtalTjFY8G8N9Ar4cRZCinTIAL1CPAZBuLIkQ6k3nrLoq0ncPd90yXuxQm4UsPZBraZCINZAz0GUUYHdD00PhzAZDZD' },
+      method: 'POST',
+      json: messageData
+
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var recipientId = body.recipient_id;
+        var messageId = body.message_id;
+
+        console.log("Successfully sent generic message with id %s to recipient %s",
+          messageId, recipientId);
+      } else {
+        console.error("Unable to send message.");
+        console.error(response);
+        console.error(error);
+      }
+    });
+  }
+};
+
+
+
+
+
+
+
