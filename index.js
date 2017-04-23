bae21fce37dcf4c69a03f9528a75d9b38b853381
Francesco Zaia <francesco.zaia@gmail.com>
2017-04-23 13:38:30 +0200

    test

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
 var bodyParser = require('body-parser'),
     path = require('path'),
     crypto = require('crypto'),
